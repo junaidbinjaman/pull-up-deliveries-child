c0021aaa86125ff38b5c6a89f5bbbd80744f5dda
junaidbinjaman <me@junaidbinjaman.com>
2024-05-02 06:54:40 +0600

    Fixed some phpcs errors

--- functions.php
+++ functions.php
@@ -5,8 +5,14 @@
  * @package astra
  */
 
- $theme = wp_get_theme();
- $version = $theme->get( 'Version' );
+/**
+ * The version of this plugin.
+ *
+ * @since    1.0.0
+ * @var      string    $version    The current version of this child theme.
+ */
+$theme   = wp_get_theme();
+$version = $theme->get( 'Version' );
 
 /**
  * Enqueue the css and js
@@ -17,7 +23,7 @@ function pull_up_deliveries_style() {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array(), $GLOBALS['version'], 'all' );
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ), $GLOBALS['version'], 'all' );
 
-	wp_enqueue_script( 'child-main-script', get_stylesheet_directory_uri() . '/scripts.js', $GLOBALS['version'], true );
+	wp_enqueue_script( 'child-main-script', get_stylesheet_directory_uri() . '/scripts.js', array( 'jquery' ), $GLOBALS['version'], true );
 }
 
 add_action( 'wp_enqueue_scripts', 'pull_up_deliveries_style' );
